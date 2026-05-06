3538c33ef878798fcd785f57af45536b0146234b
gabvfla <miyatinho@gmail.com>
2026-05-06 19:39:18 -0300

    chore: add electron main process and preload script

---
+++ electron/main.cjs
@@ -0,0 +1,57 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const fs = require('fs')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+    titleBarStyle: 'hiddenInset',
+    backgroundColor: '#0f0f13',
+    show: false,
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173')
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+
+  win.once('ready-to-show', () => win.show())
+}
+
+const dataPath = path.join(app.getPath('userData'), 'album-data.json')
+
+ipcMain.handle('load-data', () => {
+  try {
+    if (fs.existsSync(dataPath)) {
+      return JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
+    }
+    return null
+  } catch {
+    return null
+  }
+})
+
+ipcMain.handle('save-data', (_, data) => {
+  try {
+    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), 'utf-8')
+    return true
+  } catch {
+    return false
+  }
+})
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() })
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow() })
